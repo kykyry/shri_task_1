9ae495e701084c699bd46a98a80a934aaeebff8f
Maria Vasyutina <masha.vasutina.1998@gmail.com>
2022-06-15 16:58:01 +0300

    f

---
+++ s.js
@@ -0,0 +1,9 @@
+let columnCount_1 = 3;
+let elementGap = 4;
+const elem = document.querySelector("root");
+
+elem.setAttribute('column-count', 3);
+elem.setAttribute('column-gap', '4px');
+
+        // elem.style.columnCount = columnCount_1;
+        // elem.style.columnGap = `${elementGap} px`;
