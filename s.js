7fccb68abc507f9c938858bfb228e62e1d19b77f
Maria Vasyutina <masha.vasutina.1998@gmail.com>
2022-06-15 21:05:12 +0300

    f

--- s.js
+++ s.js
@@ -2,7 +2,7 @@ let columnCount = 3;
 let elementGap = 20;
 let elem = document.querySelector(".root");
 let dot_elem = document.querySelectorAll(".root .el");
-let a = (documentElement.clientWidth - (columnCount - 1)*elementGap) / columnCount;
+let a = (document.documentElement.clientWidth - (columnCount - 1)*elementGap) / columnCount;
 let arr_col = [];
 let min_index = 0;
 
