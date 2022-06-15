00e220922c701b1793e1231474c175be0f8b9881
Maria Vasyutina <masha.vasutina.1998@gmail.com>
2022-06-15 18:00:22 +0300

    f

--- s.js
+++ s.js
@@ -1,7 +1,9 @@
 let columnCount_1 = 3;
-let elementGap = 4;
+let elementGap = 20;
 let elem = document.querySelector(".root");
 let dot_elem = document.querySelectorAll(".root .el");
+let arr_dot = Array.from(dot_elem);
+arr_dot.forEach(el => console.log(el.offsetHeight))
 
 // elem.setAttribute('column-count', 3);
 // elem.setAttribute('column-gap', `${elementGap} px`);
@@ -9,6 +11,6 @@ let dot_elem = document.querySelectorAll(".root .el");
 // elem.style.columnCount = columnCount_1;
 // elem.style.columnGap = `${elementGap} px`;
 
-elem.style.cssText = `column-count: ${columnCount_1}; column-gap: ${elementGap}px;`;
-elem.offsetWidth
-dot_elem.style.cssText = `break-after: always`
+// elem.style.cssText = `column-count: ${columnCount_1}; column-gap: ${elementGap}px;`;
+elem.style.cssText = `display: grid; grid-template-columns: repeat(${columnCount_1}, 1fr); grid-gap: ${elementGap}px;`
+// elem.offsetWidth offsetHeight 
